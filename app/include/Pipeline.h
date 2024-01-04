58ceda5bec53d39d8ba63c8da10b842af627bcb3
HawkDeath <patryj1083@gmail.com>
2024-01-04 14:51:27 +0100

    [WIP] Add GraphicsPipeline class

--- app/include/Pipeline.h
+++ app/include/Pipeline.h
@@ -6,9 +6,11 @@
 #define RAYCASTING_PIPELINE_H
 
 #include <vulkan/vulkan.h>
+#include <vector>
 
 namespace gfx {
     class VulkanDevice;
+    class RenderPass;
 
     struct PipelineConfig {
         static void default_config(PipelineConfig &pipeline);
@@ -25,18 +27,32 @@ namespace gfx {
         VkPipelineColorBlendStateCreateInfo color_blend_info = {};
         VkPipelineDynamicStateCreateInfo dynamic_info = {};
         VkPipelineLayoutCreateInfo layout_info = {};
+        std::vector<VkPipelineShaderStageCreateInfo> shaders_stage;
+
     };
 
     class Pipeline {
     public:
-        explicit Pipeline(VulkanDevice &device, PipelineConfig config);
+        explicit Pipeline(VulkanDevice &device, PipelineConfig config, VkDescriptorSetLayout descriptor_set_layout);
         virtual ~Pipeline();
+
+        VkPipeline pipeline_handler() const { return m_pipeline; }
+        const PipelineConfig& pipeline_config() const { return m_pipeline_config; }
 
     protected:
         VulkanDevice &m_device;
         PipelineConfig m_pipeline_config;
 
+        VkPipelineLayout m_pipeline_layout = {};
+        VkPipelineCache m_pipeline_cache = {};
         VkPipeline m_pipeline{};
+    };
+
+    class GraphicsPipeline : public Pipeline
+    {
+    public:
+        explicit GraphicsPipeline(VulkanDevice &device, PipelineConfig config, RenderPass &render_pass, VkDescriptorSetLayout descriptor_set_layout);
+        virtual ~GraphicsPipeline() = default;
     };
 
 }
--- app/src/Pipeline.cpp
+++ app/src/Pipeline.cpp
@@ -2,7 +2,11 @@
 // Created by HawkDeath on 1/4/2024.
 //
 #include "Pipeline.h"
+
+#include <utility>
 #include "VulkanDevice.h"
+#include "RenderPass.h"
+#include "Log.h"
 
 namespace gfx {
     void PipelineConfig::default_config(PipelineConfig &pipeline) {
@@ -65,12 +69,62 @@ namespace gfx {
         pipeline.layout_info.pNext = VK_NULL_HANDLE;
     }
 
-    Pipeline::Pipeline(VulkanDevice &device, PipelineConfig config) : m_device{device}, m_pipeline_config{config} {
+    Pipeline::Pipeline(VulkanDevice &device, PipelineConfig config, VkDescriptorSetLayout descriptor_set_layout)
+            : m_device{device}, m_pipeline_config{std::move(config)} {
+        VkPipelineLayoutCreateInfo pipeline_layout_info = {};
+        pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipeline_layout_info.pNext = VK_NULL_HANDLE;
+        pipeline_layout_info.setLayoutCount = 1u;
+        pipeline_layout_info.pSetLayouts = &descriptor_set_layout;
 
+        VK_CHECK(vkCreatePipelineLayout(m_device.device_handler(), &pipeline_layout_info, nullptr, &m_pipeline_layout),
+                 "Failed to create pipeline layout");
+
+        VkPipelineCacheCreateInfo pipeline_cache_info = {};
+        pipeline_cache_info.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+        pipeline_cache_info.pNext = VK_NULL_HANDLE;
+        pipeline_cache_info.initialDataSize = 0u;
+        pipeline_cache_info.pInitialData = VK_NULL_HANDLE;
+
+        VK_CHECK(vkCreatePipelineCache(m_device.device_handler(), &pipeline_cache_info, nullptr, &m_pipeline_cache),
+                 "Failed to create pipeline cache");
     }
 
     Pipeline::~Pipeline() {
+        vkDestroyPipelineLayout(m_device.device_handler(), m_pipeline_layout, nullptr);
+        if (m_pipeline_cache)
+            vkDestroyPipelineCache(m_device.device_handler(), m_pipeline_cache, nullptr);
         if (m_pipeline)
             vkDestroyPipeline(m_device.device_handler(), m_pipeline, nullptr);
     }
+
+    GraphicsPipeline::GraphicsPipeline(VulkanDevice &device, PipelineConfig config, RenderPass &render_pass,
+                                       VkDescriptorSetLayout descriptor_set_layout) : Pipeline(device,
+                                                                                               std::move(config),
+                                                                                               descriptor_set_layout) {
+
+        VkGraphicsPipelineCreateInfo graphics_pipeline_info = {};
+        graphics_pipeline_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        graphics_pipeline_info.pNext = VK_NULL_HANDLE;
+
+        graphics_pipeline_info.stageCount = static_cast<uint32_t>(m_pipeline_config.shaders_stage.size());
+        graphics_pipeline_info.pStages = m_pipeline_config.shaders_stage.data();
+
+        graphics_pipeline_info.pVertexInputState = &m_pipeline_config.vertex_input_info;
+        graphics_pipeline_info.pInputAssemblyState = &m_pipeline_config.input_assembly_info;
+        graphics_pipeline_info.pViewportState = &m_pipeline_config.viewport_info;
+        graphics_pipeline_info.pRasterizationState = &m_pipeline_config.rasterization_info;
+        graphics_pipeline_info.pMultisampleState = &m_pipeline_config.multisample_info;
+        graphics_pipeline_info.pDepthStencilState = &m_pipeline_config.depth_stencil_info;
+        graphics_pipeline_info.pColorBlendState = &m_pipeline_config.color_blend_info;
+        graphics_pipeline_info.pDynamicState = &m_pipeline_config.dynamic_info;
+        graphics_pipeline_info.layout = m_pipeline_layout;
+        graphics_pipeline_info.renderPass = render_pass.renderpass_handler();
+        graphics_pipeline_info.subpass = 0u;
+        graphics_pipeline_info.basePipelineHandle = VK_NULL_HANDLE;
+
+        VK_CHECK(vkCreateGraphicsPipelines(m_device.device_handler(), m_pipeline_cache, 1u, &graphics_pipeline_info, nullptr, &m_pipeline), "Failed to create graphics pipeline");
+
+    }
+
 }
